c8fb3ca5bb6d8a9b2710fb603d3cca9d6bcaaabc
ElZombieIsra <dragon-zisrael@hotmail.com>
2019-07-12 19:20:51 -0500

    Route update

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 
+// Import your screens
+import 'screens/HomeScreen.dart';
+
+// Give a route and link it to your screen class
 Map<String, WidgetBuilder> routes = {    
-  // "/LoadPage": (BuildContext context) => LoaderScreen(),
+  "/": (BuildContext context) => HomeScreen(),
 };
